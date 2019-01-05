eb53bfd1fb2abf9e456b4e3331fba8a8e329a9a0
Michael Herman <hermanmu@gmail.com>
2019-01-05 11:15:10 -0700

    adds cypress

--- test.sh
+++ test.sh
@@ -19,6 +19,13 @@ docker-compose -f docker-compose-dev.yml exec client npm test -- --coverage
 inspect $? client
 docker-compose -f docker-compose-dev.yml down
 
+# run e2e tests
+docker-compose -f docker-compose-prod.yml up -d --build
+docker-compose -f docker-compose-prod.yml run users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
+inspect $? e2e
+docker-compose -f docker-compose-prod.yml down
+
 # return proper code
 if [ -n "${fails}" ]; then
   echo "Tests failed: ${fails}"
