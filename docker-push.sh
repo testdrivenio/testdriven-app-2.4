bb5a7e48d9bbb008ccd82fd120ab5697f122d266
Michael Herman <hermanmu@gmail.com>
2019-01-09 13:21:57 -0700

    testing ci

--- docker-push.sh
+++ docker-push.sh
@@ -5,10 +5,10 @@ then
 
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
-    # new
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-2001734548.us-west-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-2116729726.us-west-1.elb.amazonaws.com"
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
