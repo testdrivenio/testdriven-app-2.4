c17c3692ddd71b1f50612f83aeb953df3eae5028
Michael Herman <hermanmu@gmail.com>
2019-01-10 19:08:04 -0700

    testing ci

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -10,7 +10,7 @@ then
 
     configure_aws_cli() {
         aws --version
-        aws configure set default.region us-east-1
+        aws configure set default.region us-west-1
         aws configure set default.output json
         echo "AWS Configured!"
     }
